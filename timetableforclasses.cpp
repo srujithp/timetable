fe75d341c7e6acb19fbf1e1a60bdaa51f30576fd
Srujith Reddy Panuganti <55059610+srujithp@users.noreply.github.com>
2019-09-12 19:46:37 +0530

    Update timetableforclasses.cpp

--- timetableforclasses.cpp
+++ timetableforclasses.cpp
@@ -5,8 +5,10 @@
 struct sec
 {
     char week[7][6][20];
+    char week1[7][6][20];
 
 }s[10];
+char weekdays[10][10]={"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
 
 int main()
 {
@@ -18,16 +20,16 @@ int main()
 
 	scanf("%s",br);
 
-	printf("Enter the no. of sections : ");
+	printf("Enter the no. of sections (MAX of 6) : ");
 
 	scanf("%d",&nsec);
 
 
-	printf("Enter no. of theory subjects : ");
+	printf("Enter no. of theory subjects (MIN of 6) : ");
 
 	scanf("%d",&nthe);
 
-	printf("Enter no. of lab subjects : ");
+	printf("Enter no. of lab subjects (MAX of 6) : ");
 
 	scanf("%d",&nlab);
 
@@ -51,13 +53,65 @@ int main()
             for(k=0;k<3;k++)
             {
               if((m+l)<nlab)
-            strcpy(s[i].week[j][k],lsubs[m+l]);
+              {
+                    strcpy(s[i].week[j][k],lsubs[m+l]);
+                    strcpy(s[i].week1[j][k],nlteachers[m+l]);
+              }
             else
             {
                 m=-l;
                 strcpy(s[i].week[j][k],lsubs[m+l]);
+                strcpy(s[i].week1[j][k],nlteachers[m+l]);
             }
 
+            }
+        }
+
+    }
+    int p=0;
+    for(i=0;i<nsec;i+=2)
+    {
+        p=i;
+
+
+        for(j=nlab;j<6;j++)
+        {
+            for(k=0;k<6;k++)
+            {
+                if(p<nthe)
+                {
+                strcpy(s[i].week[j][k],tsubs[p]);
+                strcpy(s[i].week1[j][k],ntteachers[p]);
+                p++;
+                }
+                else
+                {
+                    p=0;
+                    strcpy(s[i].week[j][k],tsubs[p]);
+                    strcpy(s[i].week1[j][k],ntteachers[p]);
+                    p++;
+                }
+            }
+
+        }
+
+         for(j=0;j<nlab;j++)
+        {
+            for(k=3;k<6;k++)
+            {
+                if(p<nthe)
+                {
+                strcpy(s[i].week[j][k],tsubs[p]);
+                strcpy(s[i].week1[j][k],ntteachers[p]);
+                p++;
+                }
+                else
+                {
+                    p=0;
+                    strcpy(s[i].week[j][k],tsubs[p]);
+                    strcpy(s[i].week1[j][k],ntteachers[p]);
+                    p++;
+                }
             }
         }
 
@@ -70,11 +124,15 @@ int main()
             for(k=3;k<6;k++)
             {
             if((m+l)<nlab)
-            strcpy(s[i].week[5-j][k],lsubs[m+l]);
+            {
+                strcpy(s[i].week[5-j][k],lsubs[m+l]);
+                strcpy(s[i].week1[5-j][k],nlteachers[m+l]);
+            }
             else
             {
                 m=-l;
                 strcpy(s[i].week[5-j][k],lsubs[m+l]);
+                strcpy(s[i].week1[5-j][k],nlteachers[m+l]);
             }
 
             }
@@ -83,18 +141,81 @@ int main()
 
     }
 
+    for(i=1;i<nsec;i+=2)
+    {
+        p=i;
+        for(j=0;j<nlab;j++)
+        {
+            for(k=0;k<3;k++)
+            {
+                if(p<nthe)
+                {
+                strcpy(s[i].week[5-j][k],tsubs[p]);
+                strcpy(s[i].week1[5-j][k],ntteachers[p]);
+                p++;
+                }
+                else
+                {
+                    p=0;
+                    strcpy(s[i].week[5-j][k],tsubs[p]);
+                    strcpy(s[i].week1[5-j][k],ntteachers[p]);
+                    p++;
+                }
+            }
+        }
+
+        for(j=nlab;j<6;j++)
+        {
+            for(k=0;k<6;k++)
+            {
+                if(p<nthe)
+                {
+                strcpy(s[i].week[5-j][k],tsubs[p]);
+                strcpy(s[i].week1[5-j][k],ntteachers[p]);
+                p++;
+                }
+                else
+                {
+                    p=0;
+                    strcpy(s[i].week[5-j][k],tsubs[p]);
+                    strcpy(s[i].week1[5-j][k],ntteachers[p]);
+                    p++;
+                }
+            }
+        }
+
+    }
+
    for(i=0;i<nsec;i++)
    {
-       printf("\nsection %d\n",i);
+       printf("\nsection %c\n\n",i+65);
+       printf("             09:00        09:55        10:50        11:45        12:15        01:10        02:05        03:00\n");
+       printf("________________________________________________________________________________________________________________\n\n");
        for(j=0;j<6;j++)
        {
-           for(k=0;k<6;k++)
+           printf("%10s       ",weekdays[j]);
+           for(k=0;k<3;k++)
            {
-                printf("%s   ",s[i].week[j][k]);
+                printf("%10s   ",s[i].week[j][k]);
            }
-           printf("\n");
+           printf("     lunch   ");
+           for(k=3;k<6;k++)
+           {
+                printf("%10s   ",s[i].week[j][k]);
+           }
+           printf("\n                 ");
+            for(k=0;k<3;k++)
+           {
+                printf("%10s   ",s[i].week1[j][k]);
+           }
+             printf("             ");
+           for(k=3;k<6;k++)
+           {
+                printf("%10s   ",s[i].week1[j][k]);
+           }
+           printf("\n\n");
        }
-        printf("\n");
+        printf("\n\n\n");
    }
 
 
